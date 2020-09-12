9b04568c29120fd216478226bb9cf5a9ec689b1f
AlphaOmegaAK <alphaunionxt@gmail.com>
2020-09-12 11:43:33 -0400

    CORS, parser, logger set

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
 //  ----- Config -----
 require('dotenv').config();
 const routes = require('./routes');
 
+app.use(cors({
+    origin: ['http: localhost: 3000'],
+    methods: "GET, POST, PUT, DELETE",
+    // Precaution for browser issues
+    optionSuccessStatus: 200,
+}));
+
+//  ----- MiddleWare -----
+app, use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${new.Date().toLocaleTimeString()}`);
+})
+
+//  -----Base Route  -----
 app.use('/', routes.base);
+// ---- Auth Routes -----
+// ----- User Routes -----
+// ----- Post Routes -----
 
 // PORT + Listener
 const PORT = process.env.PORT || 5000;
